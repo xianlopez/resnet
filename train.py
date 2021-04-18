0e7bea29f5fdae627a023a0af455e6c240dd3538
xian <lopezalvarez.xian@gmail.com>
2021-04-18 12:43:14 +0200

    Trying to use Sequence as validation dataset

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ img_size = 224
 data_path = '/home/xian/ImageNet'
 batch_size = 256
 train_dataset = DataReader(data_path, batch_size, img_size, 'train')
+val_dataset = DataReader(data_path, batch_size, img_size, 'val')
 
 model = models.resnet18(img_size, train_dataset.nclasses)
 
@@ -25,7 +26,8 @@ if os.path.exists(log_dir):
     shutil.rmtree(log_dir)
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq='batch')
 
-model.fit(train_dataset, epochs=120, callbacks=[tensorboard_callback], workers=6, use_multiprocessing=True)
+model.fit(train_dataset, epochs=120, callbacks=[tensorboard_callback], workers=6, use_multiprocessing=True,
+          validation_data=val_dataset)
 
 # TODO: Data augmentation
 # TODO: Validation
